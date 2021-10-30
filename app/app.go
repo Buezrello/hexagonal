c432226ad93a1f0aabef1c3d41578bb0f115ef63
Buezrello <igindin@gmail.com>
2021-10-30 17:35:19 +0300

    Environment variables

--- app/app.go
+++ app/app.go
@@ -1,16 +1,28 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"example.com/hexagonal/domain"
 	"example.com/hexagonal/service"
 	"github.com/gorilla/mux"
 )
 
+func sanityCheck() {
+	if os.Getenv("SERVER_ADDRESS") == "" ||
+		os.Getenv("SERVER_PORT") == "" {
+
+		log.Fatal("Environment variables not defined...")
+	}
+}
+
 func Start() {
 	router := mux.NewRouter()
+
+	sanityCheck()
 
 	// writing
 	// ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
@@ -21,5 +33,23 @@ func Start() {
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
 
 	// starting server
-	log.Fatal(http.ListenAndServe("localhost:8000", router))
+	address := os.Getenv("SERVER_ADDRESS")
+	port := os.Getenv("SERVER_PORT")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", address, port), router))
+
+	// run with (if env vars not exported)
+
+	// SERVER_ADDRESS=localhost SERVER_PORT=8000 go run main.go
+
+	// or export one time env vars
+
+	// export SERVER_ADDRESS=localhost
+	// export SERVER_PORT=8000
+	// export DB_USER=root
+	// export DB_PASSWD=admin
+	// export DB_ADDR=localhost
+	// export DB_PORT=3306
+	// export DB_NAME=banking
+
+	// go run main.go
 }
