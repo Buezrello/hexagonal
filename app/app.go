929f44d982ba46f03c1a2fb1948da0b319ed30ae
Buezrello <igindin@gmail.com>
2021-10-30 20:07:04 +0300

    add new bank account

--- app/app.go
+++ app/app.go
@@ -5,10 +5,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"example.com/hexagonal/domain"
 	"example.com/hexagonal/service"
 	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
 )
 
 func sanityCheck() {
@@ -26,11 +28,16 @@ func Start() {
 
 	// writing
 	// ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryStub())}
-	ch := CustomerHandlers{service.NewCustomerService(domain.NewCustomerRepositoryDb())}
+	dbClient := getDbClient()
+	customerRepositoryDb := domain.NewCustomerRepositoryDb(dbClient)
+	accountRepositoryDb := domain.NewAccountRepositoryDb(dbClient)
+	ch := CustomerHandlers{service: service.NewCustomerService(customerRepositoryDb)}
+	ah := AccountHandler{service: service.NewAccountService(accountRepositoryDb)}
 
 	// define routes
 	router.HandleFunc("/customers", ch.getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", ch.getCustomer).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}/account", ah.NewAccount).Methods(http.MethodPost)
 
 	// starting server
 	address := os.Getenv("SERVER_ADDRESS")
@@ -52,4 +59,23 @@ func Start() {
 	// export DB_NAME=banking
 
 	// go run main.go
+}
+
+func getDbClient() *sqlx.DB {
+	dbUser := os.Getenv("DB_USER")
+	dbPasswd := os.Getenv("DB_PASSWD")
+	dbAddr := os.Getenv("DB_ADDR")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPasswd, dbAddr, dbPort, dbName)
+	client, err := sqlx.Open("mysql", dataSource)
+	if err != nil {
+		panic(err)
+	}
+	client.SetConnMaxLifetime(time.Minute * 3)
+	client.SetMaxOpenConns(10)
+	client.SetMaxIdleConns(10)
+
+	return client
 }
---
+++ dto/newAccountRequest.go
@@ -0,0 +1,25 @@
+package dto
+
+import (
+	"strings"
+
+	"example.com/hexagonal/errs"
+)
+
+type NewAccountRequest struct {
+	CustomerId  string  `json:"customer_id"`
+	AccountType string  `json:"account_type"`
+	Amount      float64 `json:"amount"`
+}
+
+func (r NewAccountRequest) Validate() *errs.AppError {
+	if r.Amount < 5000 {
+		return errs.NewValidationError("To open a new acount you need to deposite at least 5000.00")
+	}
+
+	if strings.ToLower(r.AccountType) != "saving" && strings.ToLower(r.AccountType) != "checking" {
+		return errs.NewValidationError("Account type should be checking or saving")
+	}
+
+	return nil
+}
